a719e647593f5442db1d6b40a46f66c9f12ddd43
ameliacitradewi <ameliacitradewi@gmail.com>
2024-06-11 22:45:00 +0700

    commit

---
+++ Part_2/3_combining_conditions/3.6_aplhabetically_in_the_middle.py
@@ -0,0 +1,7 @@
+# Write your solution here
+letter1 = input("1st letter:")
+letter2 = input("2nd letter:")
+letter3 = input("3rd letter:")
+
+letters = sorted([letter1, letter2, letter3])
+print(f"The letter in the middle is {letters[1]}")
