bc777737467be3e9c461aa59667760de22752aaa
ameliacitradewi <ameliacitradewi@gmail.com>
2024-06-14 23:41:16 +0700

    commit

---
+++ Part_3/2_working_with_strings/2.6_underlining.py
@@ -0,0 +1,9 @@
+# Write your solution here
+
+while True:
+    string = input("Please type in a string:")
+    if len(string) == 0:
+        break
+    else:
+        print(string)
+        print("-" * len(string))
