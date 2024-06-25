d838de80d350ca3ed82d18c81a2f48875cc57923
ameliacitradewi <ameliacitradewi@gmail.com>
2024-06-25 21:17:19 +0700

    commit

--- Part_3/2_working_with_strings/2.12_find_all_the_substring.py
+++ Part_3/2_working_with_strings/2.12_find_all_the_substring.py
@@ -1,1 +1,13 @@
-print('Hello!')
+# Write your solution here
+word = input("Please type in a word:")
+character = input("Please type in character:")
+index = word.find(character)
+
+if index != -1:
+    while index != -1:
+        if index + 3 <= len(word):
+            next_word = word[index:index + 3]
+            print(next_word)
+            index = word.find(character, index + 1)
+        else:
+            break
