ba357654606af7d58818371621f819c8462ebbe0
ameliacitradewi <ameliacitradewi@gmail.com>
2024-06-26 22:52:44 +0700

    commit

--- Part_3/3_more_loops/3.2_first_letters_of_words.py
+++ Part_3/3_more_loops/3.2_first_letters_of_words.py
@@ -1,13 +1,8 @@
 # Write your solution here
-while True:
-    user_input = int(input("Please type in a number:"))
-    number = user_input
-    result = 1
-    if number == 0 or number < 0:
-        break
-    while number > 0:
-        result *= number
-        number -= 1
-    print(f"The factorial of the number {user_input} is {result}")
-       
-print("Thanks and bye!")
+sentence = input()
+words = sentence.split(" ")
+word = 0
+
+while word < len(words):
+    print(words[word][:1])
+    word += 1
