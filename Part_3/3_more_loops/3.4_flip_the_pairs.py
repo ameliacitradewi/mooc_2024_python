4c2ceb173b169426734b2fa24e2639041ac9045b
ameliacitradewi <ameliacitradewi@gmail.com>
2024-06-26 23:20:42 +0700

    commit

--- Part_3/3_more_loops/3.4_flip_the_pairs.py
+++ Part_3/3_more_loops/3.4_flip_the_pairs.py
@@ -2,8 +2,10 @@
 number = int(input("Please type in a number:"))
 
 i = 1
+
 while i <= number:
-    if i + 1 <= number:
-        print(i + 1)
     print(i)
-    i += 2
+    if i != number:
+        print(number)
+    i += 1
+    number -= 1
